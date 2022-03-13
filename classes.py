aab297b1cf50ee2b71871d757ca1d5dc8bd6bf80
Steinz0 <asanthos@icloud.com>
2022-03-13 19:16:29 +0100

    second

--- classes.py
+++ classes.py
@@ -1,14 +1,12 @@
 from ast import Mod
 import numpy as np
 
-
 class Loss(object):
     def forward(self, y, yhat):
         pass
 
     def backward(self, y, yhat):
         pass
-
 
 class Module(object):
     def __init__(self):
@@ -69,4 +67,35 @@ class Linear(Module):
     def update_parameters(self, gradient_step=0.001):
         self._parameters -= gradient_step * self._gradient
 
+class TanH(Module):
+    def __init__(self):
+        super().__init__()
 
+    def forward(self, X):
+        return np.tanh(X)
+
+    def backward_update_gradient(self, input, delta):
+        pass
+
+    def backward_delta(self, input, delta):
+        return (1 - np.tanh(input)).T @ delta
+
+    def update_parameters(self, gradient_step=0.001):
+        return super().update_parameters(gradient_step)
+
+class Sigmoide(Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, X):
+        return 1/(1+np.exp(-X))
+
+    def backward_update_gradient(self, input, delta):
+        pass
+
+    def backward_delta(self, input, delta):
+        f = 1/(1+np.exp(-input))
+        return (f(1-f)).T @ delta
+
+    def update_parameters(self, gradient_step=0.001):
+        return super().update_parameters(gradient_step)
