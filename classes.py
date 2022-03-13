fd945713382eee4b7963d952edbe1126af3b2bbd
Steinz0 <asanthos@icloud.com>
2022-03-13 19:05:34 +0100

    first

--- classes.py
+++ classes.py
@@ -1,4 +1,4 @@
-from turtle import forward
+from ast import Mod
 import numpy as np
 
 
@@ -12,10 +12,8 @@ class Loss(object):
 
 class Module(object):
     def __init__(self):
-        self._w = None
-        self._b = None
-        self._gradient_w = None
-        self._gradient_b = None
+        self._parameters = None
+        self._gradient = None
 
     def zero_grad(self):
         ## Annule gradient
@@ -27,8 +25,7 @@ class Module(object):
 
     def update_parameters(self, gradient_step=1e-3):
         ## Calcule la mise a jour des parametres selon le gradient calcule et le pas de gradient_step
-        self._w -= gradient_step*self._gradient_w
-        self._b -= gradient_step*self._gradient_b
+        self._parameters -= gradient_step*self._gradient
 
     def backward_update_gradient(self, input, delta):
         ## Met a jour la valeur du gradient
@@ -38,26 +35,38 @@ class Module(object):
         ## Calcul la derivee de l'erreur
         pass
 
-class MSELoss(Loss):
-    def forward(self, y, yhat):
 
-        return (np.sum(y-yhat,axis=1)**2)
+class MSELoss(Loss):
+    def __init__(self) -> None:
+        super().__init__()
+    
+    def forward(self, y, yhat):
+        return np.sum(y-yhat,axis=1)**2
 
     def backward(self, y, yhat):
-        return -2 * (y - yhat)
+        return 2*np.sum(np.abs(y-yhat),axis=1)
 
 class Linear(Module):
     def __init__(self, input, output):
         super().__init__()
         self.input = input
         self.output = output
-        self._parameters = np.random.random((output,input))
+        self._parameters = np.random.random((input, output))
+        self._gradient = np.zeros((input, output))
+    
+    def forward(self, X):
+        return np.dot(X,self._parameters)
 
-    def forward(self, x):
-        return x@self._w + self._b
+    def zero_grad(self):
+        self._gradient = np.zeros((self.input,self.output))
+
+    def backward_update_gradient(self, input, delta):
+        self._gradient += input.T @ delta.reshape((delta.shape[0],-1))
 
     def backward_delta(self, input, delta):
-        pass
+        return input.T @ delta
 
+    def update_parameters(self, gradient_step=0.001):
+        self._parameters -= gradient_step * self._gradient
 
 
--- main.py
+++ main.py
@@ -1,12 +1,26 @@
+from turtle import backward
 import classes as cl
 import numpy as np
 
-x = np.random.random((50, 13))
-y = np.random.random((50, 3))
-yhat = np.random.random((50, 3))
+x = np.random.random((500, 20))
+y = np.random.random((500, 2))
 
 mse = cl.MSELoss()
 
-linear = cl.Linear(3,2)
+linear = cl.Linear(20,2)
 
-linear
+linear.forward(x)
+
+max_iter = 500
+
+for iter in range(max_iter):
+    #Calcul forward
+    yhat = linear.forward(x)
+    loss = mse.forward(y,yhat)
+    print("Iteration ",iter," : for ", loss.sum())
+    #Calcul backwards and retro-propagation
+    bsme = mse.backward(y,yhat)
+    linear.backward_update_gradient(x, bsme)
+
+    linear.update_parameters(gradient_step=10e-7)
+    linear.zero_grad()
