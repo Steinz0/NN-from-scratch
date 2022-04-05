5b40b6f4c400269d3c9e1f16c6ed3f90fd50a812
Steinz0 <asanthos@icloud.com>
2022-04-05 15:34:24 +0200

    SGD

--- classes.py
+++ classes.py
@@ -1,4 +1,4 @@
-from ast import Mod
+from tabnanny import verbose
 import numpy as np
 
 class Loss(object):
@@ -60,11 +60,11 @@ class Linear(Module):
 
     def backward_update_gradient(self, input, delta):
         delta = delta.reshape((delta.shape[0],-1))
-        self._gradient += np.matmul(input.T,delta)
+        self._gradient = self._gradient + np.dot(input.T,delta)
 
     def backward_delta(self, input, delta):
-        delta = delta.reshape((delta.shape[0],-1))
-        return np.matmul(input.T,delta)
+        #delta = delta.reshape((delta.shape[0],-1))
+        return np.matmul(delta,self._parameters.T)
 
     def update_parameters(self, gradient_step=0.001):
         self._parameters -= gradient_step * self._gradient
@@ -80,10 +80,11 @@ class TanH(Module):
         pass
 
     def backward_delta(self, input, delta):
-        return np.matmul((1 - np.tanh(input)).T, delta)
+        #delta = delta.reshape((delta.shape[0],-1))
+        return ( 1 - np.tanh(input)**2 ) * delta
 
     def update_parameters(self, gradient_step=0.001):
-        return super().update_parameters(gradient_step)
+        pass
 
 class Sigmoide(Module):
     def __init__(self):
@@ -96,8 +97,86 @@ class Sigmoide(Module):
         pass
 
     def backward_delta(self, input, delta):
+        delta = delta.reshape((delta.shape[0],-1))
         f = 1/(1+np.exp(-input))
-        return np.matmul((f*(1-f)).T, delta)
+        return (f * (1- f)) * delta
 
     def update_parameters(self, gradient_step=0.001):
-        return super().update_parameters(gradient_step)
+        pass
+
+class Sequentiel():
+    def __init__(self, layers=None) -> None:
+        self.layers = layers
+
+    def add_end_layer(self, layer):
+        self.layers.append(layer)
+
+    def add_start_layer(self, layer):
+        self.layers.insert(0, layer)
+
+    def forward(self, X):
+        self.inputs_vectors = []
+        self.inputs_vectors.append(X)
+        for ind in range(0,len(self.layers)):
+            self.inputs_vectors.append(self.layers[ind].forward(self.inputs_vectors[-1]))
+        
+        return self.inputs_vectors[-1]
+
+    def backward(self, bsme):
+        reverse_layers = self.layers[::-1]
+        reverse_vectors = self.inputs_vectors[::-1]
+        delta_val = bsme 
+        
+        #Others backwards
+        for ind in range(0,len(reverse_layers)):
+            reverse_layers[ind].backward_update_gradient(reverse_vectors[ind+1], delta_val)
+            delta_val = reverse_layers[ind].backward_delta(reverse_vectors[ind+1], delta_val)
+
+    def update_parameters(self, gradient_step=0.001):
+        for l in self.layers:
+            l.update_parameters(gradient_step=gradient_step)
+            l.zero_grad()
+
+class Optim():
+    def __init__(self, net, loss, eps=0.001) -> None:
+        self.net = net
+        self.loss = loss
+        self.eps = eps
+
+    def step(self, batch_x, batch_y, verbose=False):
+        yhat = self.net.forward(batch_x)
+        loss = self.loss.forward(batch_y, yhat)
+        if verbose:
+            print("LOSS : ", loss.sum())
+        delta_loss = self.loss.backward(batch_y, yhat)
+        self.net.backward(delta_loss)
+        self.net.update_parameters(gradient_step=self.eps)
+
+
+# def SGD(net, dataset, batch_size, nb_iter):
+#     indices = np.arange(dataset.shape[0])
+#     np.random.shuffle(indices)
+#     print(dataset.shape[0])
+#     for i in range(batch_size-1):
+        
+
+def SGD(optim, x, y, batch_size, niter=1000):
+        # Liste de variables pour simplifier la création des batchs
+        card = x.shape[0]
+        nb_batchs = card//batch_size
+        inds = np.arange(card)
+
+        # Création des batchs
+        np.random.shuffle(inds)
+        batchs = [[j for j in inds[i*batch_size:(i+1)*batch_size]] for i in range(nb_batchs)]
+
+        for i in range(niter):
+            # On mélange de nouveau lorsqu'on a parcouru tous les batchs
+            if i%nb_batchs == 0:
+                np.random.shuffle(inds)
+                batchs = [[j for j in inds[i*batch_size:(i+1)*batch_size]] for i in range(nb_batchs)]
+
+            # Mise-à-jour sur un batch
+            batch = batchs[i%(nb_batchs)]
+            print("###### BATCH ",i," ######")
+            optim.step(x[batch], y[batch], verbose=True)
--- main.py
+++ main.py
@@ -9,6 +9,7 @@ mse = cl.MSELoss()
 
 linear = cl.Linear(20,2)
 
+input()
 max_iter = 0#4000
 
 for iter in range(max_iter):
@@ -21,7 +22,7 @@ for iter in range(max_iter):
     linear.backward_update_gradient(x, bsme)
 
     #Update paramaters and grad 0 
-    linear.update_parameters(gradient_step=10e-6)
+    linear.update_parameters(gradient_step=10e-5)
     linear.zero_grad()
 
 l1 = cl.Linear(20,10)
@@ -30,7 +31,7 @@ l2 = cl.Linear(10,2)
 t2 = cl.TanH()
 s = cl.Sigmoide()
 
-max_iter2 = 4000
+max_iter2 = 0#100000
 
 for iter in range(max_iter2):
     #Calcul forward
@@ -47,14 +48,44 @@ for iter in range(max_iter2):
     loss = mse.forward(y,yhat)
     print("NN Iteration ",iter," : => LOSS : ", loss.sum())
     #Calcul backwards and retro-propagation
-    bsme = mse.backward(y,yhat)
-    bs = s.backward_delta(z2,bsme)
+    bmse = mse.backward(y,yhat)
+    bs = s.backward_delta(z2,bmse)
     bt2 = t2.backward_delta(a2,bs)
     l2.backward_update_gradient(z1,bt2)
     bl2 = l2.backward_delta(z1,bt2)
     bt1 = t1.backward_delta(a1,bl2)
     l1.backward_update_gradient(x,bt1)
-
     #Update paramaters and grad 0
-    l1.update_parameters(gradient_step=10e-6)
+    l1.update_parameters(gradient_step=0.01)
     l1.zero_grad()
+    l2.update_parameters(gradient_step=0.01)
+    l2.zero_grad()
+    
+
+
+seq = cl.Sequentiel([l1,t1,l2,t2,s])
+
+
+max_iter2 = 0#1000
+
+for iter in range(max_iter2):
+    #Calcul forward
+    yhat = seq.forward(x)
+    loss = mse.forward(y, yhat) 
+    print("NN Iteration ",iter," : => LOSS : ", loss.sum())
+    #Calcul backwards and retro-propagation
+    bmse = mse.backward(y,yhat)
+    seq.backward(bmse)
+    #Update paramaters and grad 0
+    seq.update_parameters()
+
+max_iter2 = 0#10000
+
+for iter in range(max_iter2):
+    #Calcul forward
+    op = cl.Optim(seq, mse, 0.001)
+    op.step(x, y, verbose=True)
+
+
+print("################### SGD #####################")
+cl.SGD(cl.Optim(seq, mse, 0.001), x, y, batch_size=200)
